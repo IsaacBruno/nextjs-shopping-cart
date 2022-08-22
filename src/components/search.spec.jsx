892d907d4b18c93bfe20a7d0d977fccdc0a82976
Isaac Bruno <isaacwinfor@gmail.com>
2022-08-22 08:46:44 -0300

    Ensure doSearch is called one time

--- src/components/search.spec.jsx
+++ src/components/search.spec.jsx
@@ -1,9 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Search } from './search';
 
+const doSearch = jest.fn();
+
 describe('Search', () => {
-  it('should render Search Component', () => {
-    render(<Search />);
-    expect(screen.getByTestId('search')).toBeInTheDocument();
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />);
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should call props.doSearch when form is submitted', () => {
+    render(<Search doSearch={doSearch} />);
+    const form = screen.getByRole('form');
+    fireEvent.submit(form);
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
